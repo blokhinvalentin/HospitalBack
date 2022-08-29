d4cdcf8f13f682de51841e22474de2a1be5d6f7f
blokhinvalentin <blokhin@fortech.dev>
2022-08-29 11:57:17 +0300

    editing getAllMeetings

--- src/modules/router/index.js
+++ src/modules/router/index.js
@@ -1,21 +1,7 @@
 const Router = require('express').Router;
-const userController = require('../controllers/user-controller');
 const router = new Router();
-const authMiddleware = require('../middlewares/auth-middleware');
-const {
-  getAllMeetings,
-  addMeeting,
-  deleteMeeting,
-  editMeeting
-} = require('../controllers/meeting-controller');
+const MeetingController = require('../controllers/meeting-controller');
 
-router.post('/registration', userController.registration);
-router.post('/authorization', userController.login);
-router.get('/logout', userController.logout);
-router.get('/refresh', userController.refresh);
-router.get('/meetings', getAllMeetings);
-router.post('/meetings', addMeeting);
-router.delete('/meetings/:_id', deleteMeeting);
-router.patch('/meetings/:_id', editMeeting);
+router.get('/meetings', MeetingController.getAllMeetings);
 
 module.exports = router;
